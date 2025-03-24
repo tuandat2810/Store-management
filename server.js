afb501104a51e0b5cb11602bb2975e7bbf352a03
22120061 - Tuan Dat <vttdat28102004@gmail.com>
2025-03-24 21:38:18 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Chào mừng đến với hệ thống quản lý đại lý!');
+});
+
+app.listen(port, () => {
+    console.log(`Server chạy tại http://localhost:${port}`);
+});
