53a4ee5599bf9bb306874769173344a2ccea2171
NgoPhuongDong-fithcmus <gonpro53@gmail.com>
2025-05-10 23:46:01 +0700

    fix register for admin or user

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,36 +5,15 @@ const router = express.Router();
 
 const authMiddleware = require("../middlewares/auth.middleware")
 const userController = require('../controllers/user.controller');
-
-// router.get('/', (req, res) => {
-//     res.redirect('/login');
-// })
-
-// router.get('/login', (req, res) => {
-//     res.render('login');
-// })
-
-// router.get('/register', (req, res) => {
-//     res.render('register');
-// })
-
-// router.get('/logout', (req, res) => {
-//     res.render('register');
-// })
-
-// router.post('/login', userController.login);
-// router.post('/register', userController.register);
-
-// // Route gọi API Mongo
-// router.get('/testmongo', userController.getAllUsers);
+const validation = require("../validates/client/user.validate")
 
 router.get("/register", userController.register);
 
-router.post("/register" ,userController.registerPost);
+router.post("/register", validation.registerPost, userController.registerPost);
 
 router.get("/login", userController.login);
 
-router.post("/login",userController.loginPost);
+router.post("/login",validation.loginPost, userController.loginPost);
 
 router.get("/logout", userController.logout);
 
