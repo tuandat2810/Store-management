e6b6ee8823507b736836b70076364a65805d0a15
WhoamiSexy <22120074@student.hcmus.edu.vn>
2025-05-12 01:46:50 +0700

    Sửa lỗi sidebar, và sửa animation lại

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
+
 const express = require("express");
 const router = express.Router();
 
+const authMiddleware = require("../middlewares/auth.middleware")
 const mainController = require('../controllers/main.controller');
 
 // Route AJAX: chỉ trả về HTML partial (layout: false)
 router.get('/ajax/section/:name', mainController.load_main_data_section);
 
 // Route direct: trả về full layout (layout: true hoặc có template chính)
-router.get('/section/:section', mainController.loadMainSection);
+router.get('/section/:section', authMiddleware.requireAuth, mainController.loadMainSection);
 
 module.exports = router;
