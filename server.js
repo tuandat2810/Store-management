57b31a23c374bb1146826b6b79136dfbc18ff90b
thisitapcode <icode8hoursaday@gmail.com>
2025-06-01 20:41:46 +0700

    update : animation for about page and warning login error

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ app.use(session({
 app.use(flash());
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success');
-    res.locals.error_msg = req.flash('error');
     next();
 });
 
