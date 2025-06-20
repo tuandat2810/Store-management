c4131132aa5ac7b05c4a5b3de9e944413c62fab1
22120061 - Tuan Dat <vttdat28102004@gmail.com>
2025-06-20 11:12:25 +0700

    Sửa lại trang báo cáo

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,12 +1,11 @@
 const generateRandom = require('../helpers/generateRandom');
+const { sortDistricts } = require('../helpers/sort.js');
+
 const Agency = require('../models/agency.m.js');
-const Product = require('../models/product.m.js');
 const Order = require('../models/order.m.js');
-const AgencyType = require('../models/agencytype.m.js');
 const Receipt = require('../models/receipt.m.js');
 const District = require('../models/district.m.js');
-const User = require('../models/user.m.js');
-const ProductUnit = require('../models/productunit.m.js');
+
 
 
 
@@ -31,7 +30,8 @@ module.exports.report = async (req, res) => {
     const userType = res.locals.user.type;
 
     // Lấy danh sách quận từ District model
-    const districts = await District.find().lean(); 
+    const districts = await District.find().lean();
+    const sortedDistricts = sortDistricts(districts);
 
     // Lấy tháng, năm, quận từ body hoặc mặc định
     const selectedMonth = parseInt(req.body.thang) || new Date().getMonth() + 1;
@@ -84,7 +84,7 @@ module.exports.report = async (req, res) => {
     }
 
     // Tổng doanh số toàn bộ (để tính phần trăm tỷ lệ)
-    const totalSalesAll = Object.values(salesSummary).reduce((sum, a) => sum + a.totalSales, 0); 
+    const totalSalesAll = Object.values(salesSummary).reduce((sum, a) => sum + a.totalSales, 0);
 
     // Tạo mảng báo cáo doanh thu (BM5.1)
     const salesReport = Object.entries(salesSummary).map(([code, data]) => ({
@@ -110,7 +110,7 @@ module.exports.report = async (req, res) => {
 
     // Tạo báo cáo công nợ (BM5.2)
     const debtReport = agencies.map(agency => {
-      const code = agency.agencyCode; 
+      const code = agency.agencyCode;
       const incurred = salesSummary[code]?.totalSales || 0;
       const collected = receiptMap[code] || 0;
       const openingDebt = Math.max((agency.debt || 0) - incurred + collected, 0); // gần đúng
@@ -123,11 +123,12 @@ module.exports.report = async (req, res) => {
         closingDebt: parseFloat(closingDebt.toFixed(2))
       };
     });
-    
-    const data = { districts,
+
+    const data = {
+      districts: sortedDistricts,
       salesReport, debtReport,
       selectedMonth, selectedYear, selectedDistrict,
-      months: [1,2,3,4,5,6,7,8,9,10,11,12], years: [2024, 2025]
+      months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], years: [2024, 2025]
     };
 
     res.render('bao_cao_hang_thang', {
@@ -141,125 +142,4 @@ module.exports.report = async (req, res) => {
   }
 };
 
-
-
-module.exports.load_lap_phieu_thu_tien = async (req, res) => {
-  try {
-    const receiptCode = await generateRandom.generateUniqueReceiptCode();
-    const agencies = await Agency.find().lean();
-    
-    const formattedAgencies = agencies.map(agency => {
-      const code = agency.agencyCode;
-      const name = agency.name;
-      const debt = '$' + new Intl.NumberFormat('en-US').format(agency.debt);
-
-      const paddedCode = code.padEnd(5, ' ').replace(/ /g, '\u00A0');
-      const paddedName = name.padEnd(20, ' ').replace(/ /g, '\u00A0');
-
-      return {
-        value: agency.agencyCode,
-        display: `${paddedCode} | ${paddedName} | Nợ: ${debt}`
-      };
-    });
-
-    const data = { receiptCode, formattedAgencies };
-    res.render('lap_phieu_thu_tien', {
-      layout: 'main',
-      title: 'Lập phiếu thu tiền',
-      ...data
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).render('500', { layout: false });
-  }
-};
-
-module.exports.load_xem_phieu_thu_tien = async (req, res) => {
-  try {
-    const userType = res.locals.user.type;
-    const fullname = res.locals.user.fullname;
-
-    let receipts;
-
-    if (userType === 'user') {
-      // 1. Tìm các agencyCode do user này quản lý
-      const agencies = await Agency.find({ managerUsername: fullname }, 'agencyCode').lean();
-      const agencyCodes = agencies.map(agency => agency.agencyCode);
-
-      // 2. Lọc các phiếu thu thuộc các đại lý đó
-      receipts = await Receipt.find({ agencyCode: { $in: agencyCodes } })
-        .sort({ collectionDate: -1 })
-        .lean();
-    } else {
-      // Admin thì xem tất cả
-      receipts = await Receipt.find().sort({ collectionDate: -1 }).lean();
-    }
-
-    res.render('xem_phieu_thu_tien', {
-      layout: 'main',
-      title: 'Xem phiếu thu tiền',
-      receipts
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).render('500', { layout: false });
-  }
-};
-
-
-
-module.exports.lap_phieu_thu_tienPOST = async (req, res) => {
-  try {
-    const { maphieu, agencyCode, sotien, ghichu } = req.body;
-
-    // 1. Kiểm tra dữ liệu bắt buộc
-    if (!maphieu || !agencyCode || !sotien) {
-      req.flash('error', 'Thiếu thông tin bắt buộc!');
-      return res.redirect('/main/lap_phieu_thu_tien');
-    }
-
-    const amount = parseFloat(sotien);
-    if (isNaN(amount) || amount <= 0) {
-      req.flash('error', 'Số tiền không hợp lệ!');
-      return res.redirect('/main/lap_phieu_thu_tien');
-    }
-
-    // 2. Tìm đại lý
-    const agency = await Agency.findOne({ agencyCode });
-    if (!agency) {
-      req.flash('error', 'Không tìm thấy đại lý!');
-      return res.redirect('/main/lap_phieu_thu_tien');
-    }
-
-    if (amount > agency.debt) {
-      req.flash('error', 'Số tiền thu lớn hơn số tiền đại lý đang nợ!');
-      return res.redirect('/main/lap_phieu_thu_tien');
-    }
-    // 3. Tạo phiếu thu
-    const newReceipt = new Receipt({
-      receiptCode: maphieu,
-      agencyCode: agency.agencyCode,
-      agencyName: agency.name,
-      agencyAddress: agency.address,
-      agencyPhone: agency.phone,
-      agencyEmail: agency.email,
-      collectionDate: new Date(),
-      amountCollected: amount,
-      note: ghichu || ''
-    });
-
-    await newReceipt.save();
-
-    // 4. Trừ nợ
-    agency.debt = Math.max(agency.debt - amount, 0); // Không cho âm nợ
-    await agency.save();
-
-    req.flash('success', 'Lập phiếu thu tiền thành công!');
-    return res.redirect('/main/lap_phieu_thu_tien');
-  } catch (err) {
-    console.error(err);
-    req.flash('error', 'Lập phiếu thu tiền thất bại!');
-    return res.redirect('/main/lap_phieu_thu_tien');
-  }
-};
 
